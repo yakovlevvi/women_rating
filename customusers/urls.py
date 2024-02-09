7fd7d22dae0e6cd7e1380b8418b236a94ea9b93d
Vitaliy Yakovlev <115347374+yakovlevvi@users.noreply.github.com>
2024-02-09 16:35:03 +0300

    Frontend refactor

--- customusers/urls.py
+++ customusers/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 urlpatterns = [
     path('', user_list, name='user_list'),
     path('user_ratings_list/', UserScores.as_view(), name='user_ratings_list'),
-    path('user/<str:username>/', UserProfileView.as_view(slug_field='username'), name='user_profile'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('<str:username>/', UserProfileView.as_view(slug_field='username'), name='user_profile'),
     # path('<str:username>/', ProfileView.as_view(slug_field='username'), name='user_rating'),
 ]
--- scores/utils.py
+++ scores/utils.py
@@ -5,7 +5,7 @@ from .models import *
 
 
 class DataMixin:
-    paginate_by = 5
+    paginate_by = 6
 
     def get_user_context(self, **kwargs):
         context = kwargs
